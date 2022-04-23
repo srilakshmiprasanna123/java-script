21cd7d6ecdb4ed7a486c946ded64149bde5b6d16
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-04-24 03:25:35 +0530

    added two dice numbers

--- Sequence/AddTwoDiceNumber.js
+++ Sequence/AddTwoDiceNumber.js
@@ -1,4 +1,4 @@
 let dice1 =  Math.floor((Math.random() * 6 ) + 1);
 let dice2 =  Math.floor((Math.random() * 6 ) + 1);
-let sum = num1 + num2;
+let sum = dice1 + dice2;
 console.log("Sum of dice 1 and 2 is " + sum);
