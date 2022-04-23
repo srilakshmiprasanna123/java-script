c06e72b430fd38203ae8cd479e06fa6aaa777947
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-04-24 03:14:00 +0530

    added flip coin

---
+++ Function/selection_problem_case/selection_problem_if-else/FlipCoin.js
@@ -0,0 +1,9 @@
+const randomNumber = Math.floor(( Math.random() * 2 ) + 1);
+
+//checking outcome is head or tail
+if ( randomNumber == 1 ) {
+    console.log("HEAD");
+}
+else {
+    console.log("TAIL");
+}
